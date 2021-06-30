593bae0aaddb9f4fbaaa55f067a4eb781df2d750
neondeex <30392593+neondeex@users.noreply.github.com>
2021-06-30 02:18:38 -0500

    asdf
    asdf

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -11,7 +11,7 @@
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Juega ahora: ${data.players.online} Jugadores conectados</span>`;
         {% else %}
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Estamos en linea!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;sadfjhasd, ${data.players.online} pxdsdf</p>`;
         {% endif %}
     }
     else {
