88efc7ab1d742ad1487bedbc87c31f6bb70417d4
neondeex <30392593+neondeex@users.noreply.github.com>
2021-06-30 02:20:55 -0500

    asdf
    asdf

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -7,7 +7,7 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Juega ya!: ${data.players.online} Jugando ahora</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Estamos en linea!&ensp;&middot;&ensp;${data.players.online} jugando</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Estamos en line jajajaa!&ensp;&middot;&ensp;${data.players.online} jugando</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
         {% endif %}
     }
